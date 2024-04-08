0a805f8da449466449f749214b8c28c7a56eb7e2
Beatriz <beatriz.lhopes@alcans.com.br>
2024-04-08 10:36:41 -0300

    Subindo projeto

---
+++ build/js/tokens.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 08 Apr 2024 13:22:17 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6",
+  "VariableCollectionPurple500": "#8b5cf6ff",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1fff"
+};
